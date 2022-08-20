ebe0d4f99d1bc3d6a85d13eb9897622dc8b67e22
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-08-20 02:31:18 -0500

    Clases Genericas LinkedList y Nodo

---
+++ LinkedList.java
@@ -0,0 +1,48 @@
+public class LinkedList<T>{
+    protected Nodo<T> root;
+  //Constructores
+    public LinkedList(T data) {
+        this.root = new Nodo<T>(data);
+    }
+    public LinkedList(){
+        this.root = null;
+    }
+    public Nodo<T> getRoot() {
+        return this.root;
+    }
+    public void setRoot(Nodo<T> root) {
+        this.root = root;
+    }
+  // Inserta un nodo al final
+    public void insertar(T data)	{
+        if (this.root == null)
+            this.root = new Nodo<T>(data);
+        else {
+            Nodo<T> nodoActual = root;
+            while(nodoActual.getNextNode() != null)
+                nodoActual = nodoActual.getNextNode();
+            nodoActual.setNextNode(new Nodo<T>(data));
+        }
+    }
+
+    public Nodo<T> search(T data){
+        Nodo nodoActual = root;
+        while(nodoActual!= null && !nodoActual.getData().equals(data)){
+            System.out.println(nodoActual);
+            System.out.println(data);
+            nodoActual = nodoActual.getNextNode();
+
+        }
+        return nodoActual;
+    }
+
+    public String toString() {
+        String listado = "";
+        Nodo<T> nodoActual = root;
+        while(nodoActual != null) {
+            listado += nodoActual.toString() + "  ";
+            nodoActual = nodoActual.getNextNode();
+        }
+        return listado;
+    }
+  }
