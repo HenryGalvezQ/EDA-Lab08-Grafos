20cb94b8a0ed3a9e7cd1dcb882fe58c994f7bca4
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-08-21 12:44:57 -0500

    Definiendo atributos y constructor de la clase grafo representada por lista de adyacencia

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,5 +1,7 @@
+import java.util.LinkedList;
 
 public class Grafo {
+	
 	// Clase de representación de un vértice
 	public class Vertice {
 		public int indice;
@@ -15,6 +17,23 @@ public class Grafo {
 	    public String toString() {
 	        return nombre;
 	    }
+	}
+	
+	// Atributos de la clase grafo por lista de adyacencia
+	private LinkedList<Vertice>[] lista_vertices; // arreglo de listas de adyacencia de vértices
+	private Vertice[] vertices;                   // arreglo de vértices
+	private int size;                             // número actual de vértices en la lista
+	 
+	// CONSTRUCTO DEL GRAFO
+	public Grafo(int capacidad) {
+		lista_vertices = new LinkedList[capacidad];
+		vertices = new Vertice[capacidad];
+		size = 0;
+	 
+		// Inicializando el arreglo de listas enlazadas
+		for (int i = 0; i< lista_vertices.length; i++) {
+			lista_vertices[i] = new LinkedList<Vertice>();
+		}
 	}
 	
 }
