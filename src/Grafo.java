683214f7ba8518cc825bbfb56caa14eb29e1bc56
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-08-21 12:39:27 -0500

    Creando clase interna para los vértices del grafo

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,4 +1,20 @@
 
 public class Grafo {
-
+	// Clase de representación de un vértice
+	public class Vertice {
+		public int indice;
+	    public String nombre;
+	    public boolean visitado;
+	 
+	    public Vertice(int indice, String nombre) {
+	        this.indice = indice;
+	        this.nombre = nombre;
+	        this.visitado = false;
+	    }
+	 
+	    public String toString() {
+	        return nombre;
+	    }
+	}
+	
 }
